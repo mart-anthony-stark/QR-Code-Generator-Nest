5af3c4ff922b249a1932c24f5a3169afbf473cc7
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-27 22:44:25 +0800

    Setup mongoose and env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
