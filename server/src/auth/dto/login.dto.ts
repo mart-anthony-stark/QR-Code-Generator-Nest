f51f30e35f1deec9a6fab4f0671c1bbdd060c6d5
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-28 00:20:49 +0800

    Add: login dto

---
+++ server/src/auth/dto/login.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
+
+export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+}
