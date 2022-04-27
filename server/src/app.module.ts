f52212911ed05774a792a15fe0a487f6baad6811
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-28 00:19:44 +0800

    Add: auth module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
